719ec52dab3a888f6e11937c7e1757874a57ee50
Dave Goddard <goddard.dave@gmail.com>
2012-02-19 21:03:01 -0500

    add favicon

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ end
 
 module StoryShare
   class Application < Rails::Application
-    config.application_name = "StoryShare"
+    config.application_name = "Story Share"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
